d7f6c6e8dbb8470b73e0716bd29d1937b92d5544
Andika Prihartono <97595647+andikaprihartono@users.noreply.github.com>
2022-05-29 11:29:39 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -23,20 +23,28 @@ function lakukanLooping(arrPegawai) {
 
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
-  let hasilLooping = null;
+  let hasilLooping = [];
+  dataYangAkanDilooping.forEach((e) => {
+    hasilLooping.push(`${e.namaDepan} ${e.namaBelakang}`);
+});
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
-  let jumlahPria = null;
+  let jumlahPria = 0;
+  dataYangAkanDilooping.forEach((e) => {
+    e.jenisKelamin == "M" ? jumlahPria++ : "";
+});
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
-  let jumlahWanita = null;
-
+  let jumlahWanita = 0;
+  dataYangAkanDilooping.forEach((e) => {
+    e.jenisKelamin == "F" ? jumlahWanita++ : "";
+});
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
       yang akan mengomentari apakah lebih banyak Pria atau Wanita
@@ -49,6 +57,14 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita";
+} else if (jumlahPria < jumlahWanita) {
+    komentar = "Jumlah Wanita lebih banyak dari Pria";
+} else {
+    komentar = "Jumlah Pria dan Wanita berimbang";
+}
 
   // ! JANGAN DIMODIFIKASI
   return {
